0211a835212e853a3cfa400eec0af513246c9dc5
johanfortus <fortusjohan@gmail.com>
2024-05-05 03:15:55 -0400

    finished landing page

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
     myModal.show();
   });
+
+form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
